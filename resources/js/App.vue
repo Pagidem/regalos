a70d6680b2be089d338a1ede0f0fb44da260dcc4
unknown <pagidem@gmail.com>
2025-11-05 18:36:28 -0400

    Subiendo archivos para practicar en casa

---
+++ resources/js/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <Principal />
+</template>
+<script setup>
+ import { ref } from 'vue';
+ import Principal from './Principal.vue';
+
+</script>
+<style>
+html, body {
+  min-height: 100vh;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  /* Fondo degradado tipo boda */
+  background: radial-gradient(circle at 20% 30%, rgba(107, 190, 132, 0.644), transparent 40%),
+              radial-gradient(circle at 80% 70%, rgba(202, 247, 201, 0.774), transparent 40%),
+              linear-gradient(135deg, #aadda3, #ffffffe1);
+  background-attachment: fixed;
+  font-family: 'Poppins', sans-serif;
+  color: #4a4a4a;
+}
+</style>
---
+++ resources/js/Principal.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1 class="titulo"><strong>Bienvenidos</strong></h1>
+</template>
+
+<script setup>
+
+
+
+</script>
+
+
+<style>
+/*Colores, tamaño y tipo de letra del titulo*/
+.titulo {
+    font-size: 6rem;
+    text-align: center;
+    padding: 20px;
+    color: white;
+    font-family: 'Poppins', sans-serif;
+}
+
+
+</style>
